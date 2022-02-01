07a4f8a920720199e0c1d68afe108f631f59d1f3
Joel C <joel7921111@yahoo.com>
2022-02-01 10:58:41 -0700

    further setup in js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named  "fight"
+
+function fight() {
+    window.alert("The fight  has  begun!");
+}
+
+// fight();
+
+var playerName = window.prompt("What is your name?");
+// note the lack of quotation marks around playerName
+
+//what is this? v
+console.log(playerName);
+
+console.log("this  logs a string, good for leaving yourelf a msg");
+//this will do math and log 20
+console.log(10+10);
+
+//what is  this v
+console.log("Our robot's  name is " + playerName)
